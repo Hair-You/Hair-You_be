060694f4e9ed5f4939349cf0104e5f07c387953d
gu <82009882+kugu98@users.noreply.github.com>
2023-09-06 17:30:28 +0900

    be_setting

---
+++ demo/src/main/java/Hairyou/demo/HairYouBeApplication.java
@@ -0,0 +1,13 @@
+package Hairyou.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairYouBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairYouBeApplication.class, args);
+	}
+
+}
